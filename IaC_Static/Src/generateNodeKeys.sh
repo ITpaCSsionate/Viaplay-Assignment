eeab0dc7bf7ddfefe86c981edae135ea234390c8
ITpaCSsionate <garrotealberto99@gmail.com>
2022-09-24 13:20:35 +0200

    Solved small buggy

--- IaC_Static/Src/generateNodeKeys.sh
+++ IaC_Static/Src/generateNodeKeys.sh
@@ -2,5 +2,5 @@
 ssh-keygen -q -N '' -t rsa -f Nodesstatic
 ssh-keygen -q -N '' -t rsa -f Bastionstatic
 
-mv Nodes Nodesstatic.privkey
-mv Bastion Bastionstatic.privkey
+mv Nodesstatic Nodesstatic.privkey
+mv Bastionstatic Bastionstatic.privkey
